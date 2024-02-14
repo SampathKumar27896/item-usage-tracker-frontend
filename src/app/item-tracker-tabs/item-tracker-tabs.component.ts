9007721831623498b7b31068f4f91c3c03877c35
Sampathkumar27896 <sampathkumar27896@hotmail.com>
2024-02-15 00:09:17 +0530

    Added a tool bar

---
+++ src/app/item-tracker-tabs/item-tracker-tabs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-tracker-tabs',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-tracker-tabs.component.html',
+  styleUrl: './item-tracker-tabs.component.css'
+})
+export class ItemTrackerTabsComponent {
+
+}
