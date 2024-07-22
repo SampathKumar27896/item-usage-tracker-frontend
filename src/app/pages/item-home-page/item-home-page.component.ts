ab5478ebbb820664d3138d3cf66a9791b2bd09bc
KeyboardScript <sampathkumar27896@hotmail.com>
2024-07-22 12:49:33 +0000

    Added few enhancements

--- src/app/pages/item-home-page/item-home-page.component.ts
+++ src/app/pages/item-home-page/item-home-page.component.ts
@@ -3,7 +3,7 @@ import  { AddItemButtonComponent } from '../../components/add-item-button/add-it
 import { RouterLink } from '@angular/router';
 import { ItemAddFormComponent } from '../../components/item-add-form/item-add-form.component';
 import {MatCardModule} from '@angular/material/card';
-
+import Item  from './item.interface';
 @Component({
   selector: 'app-item-home-page',
   standalone: true,
@@ -15,6 +15,31 @@ import {MatCardModule} from '@angular/material/card';
   styleUrl: './item-home-page.component.scss',
 })
 export class ItemHomePageComponent {
-  cardsList: string[] = [];
+  cardsList: Item[] = [
+    {
+      name: "Shirt",
+      description: "The yellow colored shirt"
+    },
+    {
+      name: "Phone",
+      description: "POCO F5"
+    },
+    {
+      name: "Charger",
+      description: "The charger for the phone POCO F5"
+    },
+    {
+      name: "Wallet",
+      description: "Money wallet"
+    },
+    {
+      name: "Headphones",
+      description: "Headphones for listening music"
+    },
+    {
+      name: "Plate",
+      description: "Plate used to eat meals"
+    }
+  ];
   
 }
