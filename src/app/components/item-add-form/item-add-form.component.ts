d2e74a42f734a7bc37f58f9bb779de1e027bbc73
KeyboardScript <sampathkumar27896@hotmail.com>
2024-07-22 09:23:54 +0000

    Added small enhancements

--- src/app/components/item-add-form/item-add-form.component.ts
+++ src/app/components/item-add-form/item-add-form.component.ts
@@ -1,15 +1,21 @@
 import { Component, Input, OnChanges, SimpleChanges, EventEmitter, Output } from '@angular/core';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms';
+import {FormGroup, FormControl, ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 @Component({
   selector: 'app-item-add-form',
   standalone: true,
-  imports: [MatInputModule, MatFormFieldModule, FormsModule, MatButtonModule],
+  imports: [MatInputModule, MatFormFieldModule, MatButtonModule, ReactiveFormsModule],
   templateUrl: './item-add-form.component.html',
   styleUrl: './item-add-form.component.scss',
 })
 export class ItemAddFormComponent {
- 
+  addItemFormGroup = new FormGroup({ 
+    name: new FormControl(''),
+    description: new FormControl(''),
+  });
+  onSubmit() {
+    console.log(this.addItemFormGroup.value);
+  }
 }
--- src/app/pages/item-home-page/item-home-page.component.ts
+++ src/app/pages/item-home-page/item-home-page.component.ts
@@ -2,14 +2,19 @@ import { Component, SimpleChanges, Input, OnChanges } from '@angular/core';
 import  { AddItemButtonComponent } from '../../components/add-item-button/add-item-button.component';
 import { RouterLink } from '@angular/router';
 import { ItemAddFormComponent } from '../../components/item-add-form/item-add-form.component';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-item-home-page',
   standalone: true,
-  imports: [AddItemButtonComponent, RouterLink, ItemAddFormComponent],
+  imports: [AddItemButtonComponent,
+     RouterLink, 
+     ItemAddFormComponent,
+     MatCardModule],
   templateUrl: './item-home-page.component.html',
   styleUrl: './item-home-page.component.scss',
 })
 export class ItemHomePageComponent {
-
+  cardsList: string[] = [];
+  
 }
